721ea4fa96cb19d7a299069b8a430531fd6101e6
Sammy Barasa <barsamms@gmail.com>
2020-07-21 20:54:03 +0300

    Updates

--- book.php
+++ book.php
@@ -42,7 +42,7 @@ $scheduledMasses = $conn->getActiveScheduledMasses($schedule_id);
 <div class="container-fluid h-100">
 
     <!-- success banner -->
-    <div class="row h-100 justify-content-center align-items-center hiddens">
+    <div class="row h-100 justify-content-center align-items-center hidden">
         <div class="col-10 col-md-8 col-lg-6">
             <div class="card" id="success-card">
                 <div class="thank-you-pop card-body">
@@ -59,162 +59,165 @@ $scheduledMasses = $conn->getActiveScheduledMasses($schedule_id);
     <!-- end success banner-->
 
     <!-- input form -->
-    <div class="card hidden" id="mass-card">
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-10 col-md-12 col-lg-10">
+            <div class="card" id="mass-card">
 
-        <h5 class="card-header text-center text-white bg-primary">
-            <strong>Register</strong>
-        </h5>
+                <h5 class="card-header text-center text-white bg-primary">
+                    <strong>Register</strong>
+                </h5>
 
-        <!--Card content-->
-        <div class="card-body">
-            <!-- Form -->
-            <form action="#" id="mass-reg-form" class="needs-validation" data-parsley-validate="">
+                <!--Card content-->
+                <div class="card-body">
+                    <!-- Form -->
+                    <form action="#" id="mass-reg-form" class="needs-validation" data-parsley-validate="">
 
-                <input type="hidden" id="schedule_id" name="schedule_id" value="<?= $schedule_id ?>" readonly>
-                <input type="hidden" id="outstation_id" name="outstation_id" value="<?= $station_id ?>"
-                       readonly>
+                        <input type="hidden" id="schedule_id" name="schedule_id" value="<?= $schedule_id ?>" readonly>
+                        <input type="hidden" id="outstation_id" name="outstation_id" value="<?= $station_id ?>"
+                               readonly>
 
-                <!-- Email -->
-                <div class="row">
-                    <div class="col-md">
-                        <div class="form-group">
-                            <label for="surname">Surname</label>
-                            <input type="text" id="surname" name="surname" class="form-control" required>
-                            <div class="invalid-feedback">Please fill out this field.</div>
-                        </div>
-                    </div>
-                    <!-- Password -->
-                    <div class="col-md">
-                        <div class="form-group">
-                            <label for="other_names">Other names</label>
-                            <input type="text" id="other_names" name="other_names" class="form-control"
-                                   required>
-                            <div class="invalid-feedback">Please fill out this field.</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md">
-                        <label>Are you an adult?</label>
-                        <div class="form-group">
-                            <div class="form-check-inline">
-                                <label class="form-check-label">
-                                    <input type="radio" class="form-check-input" name="adultFlag" value="1"
-                                           required>Yes
-                                </label>
-                                <div class="invalid-feedback">Please fill out this field.</div>
-                            </div>
-                            <div class="form-check-inline">
-                                <label class="form-check-label">
-                                    <input type="radio" class="form-check-input" name="adultFlag" value="0"
-                                           required>No
-                                </label>
-                                <div class="invalid-feedback">Please fill out this field.</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md">
-                        <div class="form-group">
-                            <label for="age">What is your age?</label>
-                            <input type="number" id="age" name="age" class="form-control" required>
-                            <div class="invalid-feedback">Please fill out this field.</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md">
-                        <div class="form-group">
-                            <label for="mobile">What is your mobile number?</label>
-                            <input type="text" id="mobile" name="mobile" class="form-control" required>
-                            <div class="invalid-feedback">Please fill out this field.</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md">
-                        <div class="form-group">
-                            <label for="group-id">What is the name of your jumuia?</label>
-                            <select class="form-control" id="group-id" name="group_id" required>
-                                <option value="" selected>Select your jumuia</option>
-                                <?php foreach ($groups as $key => $value): ?>
-                                    <option value="<?= $value['group_id'] ?>"><?= $value['group_name'] ?></option>
-                                <?php endforeach; ?>
-                            </select>
-                            <?php foreach ($groups as $key => $value):
-                                $id = $value['group_id'];
-                                $capacity = $value['estate_id'];
-                                ?>
-                                <input type="hidden" id="estate-<?= $id ?>" value="<?= $capacity ?>"
-                                       class="form-control" readonly>
-                            <?php endforeach; ?>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md">
-                        <div class="form-group">
-                            <label for="estate-name">What is your estate name?</label>
-                            <input type="text" id="estate_name" name="estate_name" class="form-control"
-                                   required>
-                            <div class="invalid-feedback">Please fill out this field.</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md">
-                        <h5>Choose your preferred mass</h5>
-                        <div class="funkyradio form-group">
-                            <?php foreach ($scheduledMasses as $key => $value):
-                                $id = $value['id'];
-                                $massId = $value['mass_id'];
-                                $capacity = $value['capacity'];
-                                $seatsLeft = $conn->getSeatsLeft($massId, $capacity);
-
-                                $disabled = $seatsLeft <= 0 ? 'disabled' : '';
-                                ?>
-                                <div class="funkyradio-success">
-                                    <input type="radio" name="mass_schedule" class="mass_schedule"
-                                           id="defaultChecked-<?= $key ?>"
-                                           value="<?= $value['id'] ?>" required <?= $disabled ?>/>
-                                    <label for="defaultChecked-<?= $key ?>">
-                                        <?= trim($value['mass_title']) ?>
-                                    </label>
-                                    <span class="float-right mx-1 badge badge-info"
-                                          id="seats-left-<?= $id ?>"><?= $seatsLeft ?> seats</span>
+                        <!-- Email -->
+                        <div class="row">
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="surname">Surname</label>
+                                    <input type="text" id="surname" name="surname" class="form-control" required>
                                     <div class="invalid-feedback">Please fill out this field.</div>
                                 </div>
-                            <?php endforeach; ?>
-                            <?php foreach ($scheduledMasses as $key => $value):
-                                $id = $value['id'];
-                                $capacity = $value['capacity'];
-                                ?>
-                                <input type="hidden" id="mass-capacity-<?= $id ?>" value="<?= $capacity ?>"
-                                       readonly>
-                            <?php endforeach; ?>
-                            <input type="hidden" id="mass-capacity" name="mass_capacity" readonly>
+                            </div>
+                            <!-- Password -->
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="other_names">Other names</label>
+                                    <input type="text" id="other_names" name="other_names" class="form-control"
+                                           required>
+                                    <div class="invalid-feedback">Please fill out this field.</div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+
+                        <div class="row">
+                            <div class="col-md">
+                                <label>Are you an adult?</label>
+                                <div class="form-group">
+                                    <div class="form-check-inline">
+                                        <label class="form-check-label">
+                                            <input type="radio" class="form-check-input" name="adultFlag" value="1"
+                                                   required>Yes
+                                        </label>
+                                        <div class="invalid-feedback">Please fill out this field.</div>
+                                    </div>
+                                    <div class="form-check-inline">
+                                        <label class="form-check-label">
+                                            <input type="radio" class="form-check-input" name="adultFlag" value="0"
+                                                   required>No
+                                        </label>
+                                        <div class="invalid-feedback">Please fill out this field.</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="age">What is your age?</label>
+                                    <input type="number" id="age" name="age" class="form-control" required>
+                                    <div class="invalid-feedback">Please fill out this field.</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="mobile">What is your mobile number?</label>
+                                    <input type="text" id="mobile" name="mobile" class="form-control" required>
+                                    <div class="invalid-feedback">Please fill out this field.</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="group-id">What is the name of your jumuia?</label>
+                                    <select class="form-control" id="group-id" name="group_id" required>
+                                        <option value="" selected>Select your jumuia</option>
+                                        <?php foreach ($groups as $key => $value): ?>
+                                            <option value="<?= $value['group_id'] ?>"><?= $value['group_name'] ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                    <?php foreach ($groups as $key => $value):
+                                        $id = $value['group_id'];
+                                        $capacity = $value['estate_id'];
+                                        ?>
+                                        <input type="hidden" id="estate-<?= $id ?>" value="<?= $capacity ?>"
+                                               class="form-control" readonly>
+                                    <?php endforeach; ?>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="estate-name">What is your estate name?</label>
+                                    <input type="text" id="estate_name" name="estate_name" class="form-control"
+                                           required>
+                                    <div class="invalid-feedback">Please fill out this field.</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md">
+                                <h5>Choose your preferred mass</h5>
+                                <div class="funkyradio form-group">
+                                    <?php foreach ($scheduledMasses as $key => $value):
+                                        $id = $value['id'];
+                                        $massId = $value['mass_id'];
+                                        $capacity = $value['capacity'];
+                                        $seatsLeft = $conn->getSeatsLeft($massId, $capacity);
+
+                                        $disabled = $seatsLeft <= 0 ? 'disabled' : '';
+                                        ?>
+                                        <div class="funkyradio-success">
+                                            <input type="radio" name="mass_schedule" class="mass_schedule"
+                                                   id="defaultChecked-<?= $key ?>"
+                                                   value="<?= $value['id'] ?>" required <?= $disabled ?>/>
+                                            <label for="defaultChecked-<?= $key ?>">
+                                                <?= trim($value['mass_title']) ?>
+                                            </label>
+                                            <span class="float-right mx-1 badge badge-info"
+                                                  id="seats-left-<?= $id ?>"><?= $seatsLeft ?> seats</span>
+                                            <div class="invalid-feedback">Please fill out this field.</div>
+                                        </div>
+                                    <?php endforeach; ?>
+                                    <?php foreach ($scheduledMasses as $key => $value):
+                                        $id = $value['id'];
+                                        $capacity = $value['capacity'];
+                                        ?>
+                                        <input type="hidden" id="mass-capacity-<?= $id ?>" value="<?= $capacity ?>"
+                                               readonly>
+                                    <?php endforeach; ?>
+                                    <input type="hidden" id="mass-capacity" name="mass_capacity" readonly>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Register button -->
+                        <button class="btn btn-outline-success btn-rounded btn-block waves-effect" type="button"
+                                id="btn-register">
+                            Register
+                        </button>
+
+                    </form>
+                    <!-- Form -->
+
                 </div>
-                <!-- Register button -->
-                <button class="btn btn-outline-success btn-rounded btn-block waves-effect" type="button"
-                        id="btn-register">
-                    Register
-                </button>
 
-            </form>
-            <!-- Form -->
-
+            </div>
         </div>
-
     </div>
-    <!-- Material form login -->
     <!-- end of input form -->
 </div>
 <!-- /Start your project here-->
