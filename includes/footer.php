ce03bb4ec440ff179d5a1cf6e4f07f1783fbe9d7
Sammy Barasa <barsamms@gmail.com>
2020-07-24 11:17:35 +0300

    Updated popper version to remove syntax error

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 <!-- JQuery -->
 <script type="text/javascript" src="vendor/yarn-asset/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap tooltips -->
-<script type="text/javascript" src="vendor/yarn-asset/popper.js/dist/popper.min.js"></script>
+<script type="text/javascript" src="vendor/yarn-asset/popper.js/dist/umd/popper.min.js"></script>
 <!-- Bootstrap core JavaScript -->
 <script type="text/javascript" src="vendor/yarn-asset/bootstrap/dist/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="vendor/yarn-asset/sweetalert/dist/sweetalert.min.js"></script>
