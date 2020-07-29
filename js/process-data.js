9e44e8618dad9d10f5d6e0c7ceb4066553b164d9
Sammy Barasa <barsamms@gmail.com>
2020-07-29 22:14:07 +0300

    excluded adult o the selected radio button

--- js/process-data.js
+++ js/process-data.js
@@ -72,8 +72,8 @@ jQuery(document).ready(function () {
         jQuery('#surname').val(null).prop("readonly", false);
         jQuery('#other_names').val(null).prop("readonly", false);
         jQuery('#age').val(null);
-        // $('input[name="adultFlag"]').prop('checked', false);
-        $('input[name="genderFlag"]').prop('checked', false);
+        //$('input[name="adultFlag"]').prop('checked', false);
+        jQuery('input[name="genderFlag"]').prop('checked', false);
         jQuery('.prefill-section').slideDown();
         jQuery('#multiRecordModal').modal('hide');
     });
